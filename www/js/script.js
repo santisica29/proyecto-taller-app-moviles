f7ab2f8121cdbafee1fc1d263915743d2ed94b60
Santi Sica <santisica29@gmail.com>
2024-02-19 20:54:42 -0300

    made filter search for registered food intake

--- www/js/script.js
+++ www/js/script.js
@@ -50,6 +50,10 @@ function eventos() {
     document
         .querySelector("#btnRegistrarComida")
         .addEventListener("click", registrarAlimento);
+
+    // //document.querySelector("#btnFiltrarLista").addEventListener('click', () => {
+    //     alert('hola')
+    // })
 }
 
 function tomarDatosLogin() {
@@ -158,13 +162,15 @@ async function registrar(user, pw, pais, cals) {
 }
 
 async function mostrarCalorias() {
-
-    mostrarLoader('Obteniendo calorias');
+    mostrarLoader("Obteniendo calorias");
     await getCalorias();
     detenerLoader();
 
-    document.querySelector("#mostrarCalorias").innerHTML = `<ion-list><ion-item>Meta diaria de calorias: ${localStorage.getItem(
-        "calorias")}</ion-item></ion-list>
+    document.querySelector(
+        "#mostrarCalorias"
+    ).innerHTML = `<ion-list><ion-item>Meta diaria de calorias: ${localStorage.getItem(
+        "calorias"
+    )}</ion-item></ion-list>
         <ion-list>
         <ion-item color=${calcCaloriasQueFaltanParaLlegarAlLimite()}>Calorias de hoy: ${caloriasDelDia} (${calcRestanteOSobranteDeCalorias()})</ion-item></ion-list>
         <ion-list><ion-item>Calorias totales registradas: ${caloriasTotales}</ion-item></ion-list>`;
@@ -174,17 +180,20 @@ function calcCaloriasQueFaltanParaLlegarAlLimite() {
     let metaDiaria = localStorage.getItem("calorias");
     if (caloriasDelDia > metaDiaria) {
         return "danger";
-    } else if (caloriasDelDia >= metaDiaria * 0.9 && caloriasDelDia <= metaDiaria) {
+    } else if (
+        caloriasDelDia >= metaDiaria * 0.9 &&
+        caloriasDelDia <= metaDiaria
+    ) {
         return "warning";
     } else {
         return "success";
     }
 }
 
-function calcRestanteOSobranteDeCalorias(){
+function calcRestanteOSobranteDeCalorias() {
     let metaDiaria = localStorage.getItem("calorias");
 
-    if (caloriasDelDia < metaDiaria){
+    if (caloriasDelDia < metaDiaria) {
         return `+ ${metaDiaria - caloriasDelDia}`;
     } else {
         return `- ${caloriasDelDia - metaDiaria}`;
@@ -225,6 +234,7 @@ function navegar(e) {
     } else if (RUTA == "/cargar-alimentos") {
         CARGARALIMENTOS.style.display = "block";
         poblarSelectAlimentos();
+        fijarFecha();
     } else if (RUTA == "/lista-alimentos") {
         LISTARALIMENTOS.style.display = "block";
         getListaAlimentos();
@@ -241,8 +251,8 @@ async function registrarAlimento() {
     let cantidad = Number(document.querySelector("#cantidadAlimento").value);
     let fecha = document.querySelector("#calendario").value;
 
-    if (idAlimento === "" || cantidad === "") {
-        mostrarToast("Todos los datos son obligatorios");
+    if (idAlimento === undefined || cantidad === 0) {
+        mostrarToast("Todos los datos son obligatorios", 3500);
         return;
     }
 
@@ -316,7 +326,11 @@ async function poblarSelectAlimentos() {
         const data = await response.json();
 
         for (let alimento of data.alimentos) {
-            slcAlimentos.innerHTML += `<ion-select-option value="${alimento.id}">${alimento.nombre} (${alimento.porcion.substring(alimento.porcion.length - 1)})</ion-select-option>`;
+            slcAlimentos.innerHTML += `<ion-select-option value="${
+                alimento.id
+            }">${alimento.nombre} (${alimento.porcion.substring(
+                alimento.porcion.length - 1
+            )})</ion-select-option>`;
         }
         detenerLoader();
     } catch (err) {
@@ -348,7 +362,7 @@ async function getListaAlimentos() {
     let idUser = localStorage.getItem("idUser");
 
     try {
-        mostrarLoader('Cargando lista de alimentos.')
+        mostrarLoader("Cargando lista de alimentos.");
         const response = await fetch(
             `https://calcount.develotion.com/registros.php?idUsuario=${idUser}`,
             {
@@ -361,7 +375,7 @@ async function getListaAlimentos() {
         );
 
         const data = await response.json();
-
+        console.log(data);
         const responseAlimentos = await fetch(
             "https://calcount.develotion.com/alimentos.php",
             {
@@ -379,8 +393,21 @@ async function getListaAlimentos() {
         if (data.registros.length < 1) {
             lista.innerHTML = "No tiene registros hechos.";
         } else {
-            lista.innerHTML = "";
-            for (let comida of data.registros) {
+            lista.innerHTML = `<ion-item>
+            <ion-datetime-button datetime="fecha1"></ion-datetime-button>
+            <ion-modal>
+              <ion-datetime id="fecha1" presentation="date" show-default-buttons="true" done-text="Confirmar" cancel-text="Cancelar">
+              </ion-datetime>
+            </ion-modal>
+            <ion-datetime-button datetime="fecha2"></ion-datetime-button>
+            <ion-modal>
+              <ion-datetime id="fecha2" presentation="date" show-default-buttons="true" done-text="Confirmar" cancel-text="Cancelar">
+              </ion-datetime>
+            </ion-modal>
+          <ion-button color="dark-purple" onclick="getListaFiltradaAlimentos()">Filtrar busqueda</ion-button>
+          </ion-item>`;
+            listaAMostrar = ordenarListaPorFechas(data.registros);
+            for (let comida of listaAMostrar) {
                 for (let c of alimentos.alimentos) {
                     if (comida.idAlimento === c.id) {
                         let caloriasDeAlimento = calcCalorias(
@@ -388,16 +415,20 @@ async function getListaAlimentos() {
                             c.porcion,
                             c.calorias
                         );
+                        let fecha =
+                            comida.fecha === getFechaActual()
+                                ? "Hoy"
+                                : comida.fecha;
                         lista.innerHTML += `
                         <ion-card color="light-purple">
             <ion-card-header>
-              <ion-card-subtitle>${comida.fecha}</ion-card-subtitle>
+              <ion-card-subtitle>${fecha}</ion-card-subtitle>
               <ion-card-title><img src="https://calcount.develotion.com/imgs/${c.imagen}.png" /> ${c.nombre}</ion-card-title>
             </ion-card-header>
             <ion-card-content>
               Calorias: ${caloriasDeAlimento}
             </ion-card-content>
-            <ion-button id="${comida.id}" onclick="eliminarRegistro(this.id)">Eliminar Registro</ion-button>
+            <ion-button id="${comida.id}" onclick="eliminarRegistro(this.id)" color="dark-purple">Eliminar Registro</ion-button>
           </ion-card>`;
                     }
                 }
@@ -408,21 +439,179 @@ async function getListaAlimentos() {
     }
 }
 
-async function getCalorias() {
+async function getListaFiltradaAlimentos() {
+    let lista = document.getElementById("lista-alimentos");
     let idUser = localStorage.getItem("idUser");
-    let apiKey = localStorage.getItem("token");
-    let headers = {headers:{
-        "Content-Type": "application/json",
-        apikey: apiKey,
-        iduser: idUser,
-    }}
+    let f1 = document.querySelector("#fecha1").value;
+    let f2 = document.querySelector("#fecha2").value;
+
+    if (f1 === undefined && f2 === undefined) {
+        f1 = getFechaActual();
+        f2 = getFechaActual();
+    } else if (f1 === undefined) {
+        f1 = getFechaActual();
+    } else if (f2 === undefined) {
+        f2 = getFechaActual();
+    }
+
+    if (f1 != getFechaActual() && f2 != getFechaActual()) {
+        f1 = recortarFecha(f1);
+        f2 = recortarFecha(f2);
+    } else if (f1 != getFechaActual()) {
+        f1 = recortarFecha(f1);
+    } else if (f2 != getFechaActual()) {
+        f2 = recortarFecha(f2);
+    }
+
+    mostrarLoader("Cargando lista filtrada.");
+    let listaFiltrada = await getListaFiltrada(f1, f2);
+
+    if (listaFiltrada.length < 1) {
+        lista.innerHTML = "No tiene registros hechos en esas fechas.";
+        detenerLoader();
+        return;
+    }
+
+    const responseAlimentos = await fetch(
+        "https://calcount.develotion.com/alimentos.php",
+        {
+            headers: {
+                "Content-Type": "application/json",
+                apikey: localStorage.getItem("token"),
+                iduser: idUser,
+            },
+        }
+    );
+
+    const alimentos = await responseAlimentos.json();
+
+    detenerLoader();
+    lista.innerHTML = `<ion-item>
+            <ion-datetime-button datetime="fecha1"></ion-datetime-button>
+            <ion-modal>
+              <ion-datetime id="fecha1" presentation="date" show-default-buttons="true" done-text="Confirmar" cancel-text="Cancelar">
+              </ion-datetime>
+            </ion-modal>
+            <ion-datetime-button datetime="fecha2"></ion-datetime-button>
+            <ion-modal>
+              <ion-datetime id="fecha2" presentation="date" show-default-buttons="true" done-text="Confirmar" cancel-text="Cancelar">
+              </ion-datetime>
+            </ion-modal>
+          <ion-button color="dark-purple" onclick="getListaFiltradaAlimentos()">Filtrar busqueda</ion-button>
+          </ion-item>`;
+    for (let comida of listaFiltrada) {
+        for (let c of alimentos.alimentos) {
+            if (comida.idAlimento === c.id) {
+                let caloriasDeAlimento = calcCalorias(
+                    comida.cantidad,
+                    c.porcion,
+                    c.calorias
+                );
+                let fecha =
+                    comida.fecha === getFechaActual() ? "Hoy" : comida.fecha;
+                lista.innerHTML += `
+                            <ion-card color="light-purple">
+                <ion-card-header>
+                  <ion-card-subtitle>${fecha}</ion-card-subtitle>
+                  <ion-card-title><img src="https://calcount.develotion.com/imgs/${c.imagen}.png" /> ${c.nombre}</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  Calorias: ${caloriasDeAlimento}
+                </ion-card-content>
+                <ion-button id="${comida.id}" onclick="eliminarRegistro(this.id)" color="dark-purple">Eliminar Registro</ion-button>
+              </ion-card>`;
+            }
+        }
+    }
+}
+
+async function getListaFiltrada(f1, f2) {
+    let idUser = localStorage.getItem("idUser");
+    let listaFiltrada = [];
 
     try {
-        const response = await fetch(`https://calcount.develotion.com/registros.php?idUsuario=${idUser}`, headers);
+        const response = await fetch(
+            `https://calcount.develotion.com/registros.php?idUsuario=${idUser}`,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    apikey: localStorage.getItem("token"),
+                    iduser: idUser,
+                },
+            }
+        );
 
         const data = await response.json();
 
-        const responseAlimentos = await fetch("https://calcount.develotion.com/alimentos.php", headers);
+        for (let comida of data.registros) {
+            let fechaDelRegistro = volverFechaANum(comida.fecha);
+            let fechaMenor = volverFechaANum(f1);
+            let fechaMayor = volverFechaANum(f2);
+            if (
+                fechaDelRegistro >= fechaMenor &&
+                fechaDelRegistro <= fechaMayor
+            ) {
+                listaFiltrada.push(comida);
+            }
+        }
+    } catch (err) {
+        mostrarLoader(err, 6000);
+    }
+
+    return listaFiltrada;
+}
+
+function ordenarListaPorFechas(lista) {
+    let listaOrdenada = lista.sort((a, b) => a - b);
+
+    return listaOrdenada;
+}
+
+function volverFechaANum(fecha) {
+    let num = 0;
+
+    for (let i = 0; i < fecha.length; i++) {
+        if (fecha[i] != "-") {
+            num += fecha[i];
+        }
+    }
+
+    return Number(num);
+}
+
+function fijarFecha() {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+    document
+        .querySelector("#calendario")
+        .setAttribute("max", `${year}-${month}-${day}T23:59:59`);
+}
+
+async function getCalorias() {
+    let idUser = localStorage.getItem("idUser");
+    let apiKey = localStorage.getItem("token");
+    let headers = {
+        headers: {
+            "Content-Type": "application/json",
+            apikey: apiKey,
+            iduser: idUser,
+        },
+    };
+
+    try {
+        const response = await fetch(
+            `https://calcount.develotion.com/registros.php?idUsuario=${idUser}`,
+            headers
+        );
+
+        const data = await response.json();
+
+        const responseAlimentos = await fetch(
+            "https://calcount.develotion.com/alimentos.php",
+            headers
+        );
 
         const alimentos = await responseAlimentos.json();
 
@@ -450,7 +639,7 @@ async function getCalorias() {
 
 function calcCalorias(cantidad, unidad, calorias) {
     let porcion = unidad.substring(0, unidad.length - 1);
-    return ((cantidad / porcion) * calorias)
+    return Math.round((cantidad / porcion) * calorias);
 }
 
 async function eliminarRegistro(id) {
@@ -482,6 +671,7 @@ function cerrarMenu() {
 }
 
 function ocultarTodo() {
+    HOME.style.display = "none";
     LOGIN.style.display = "none";
     REGISTRO.style.display = "none";
     CARGARALIMENTOS.style.display = "none";
